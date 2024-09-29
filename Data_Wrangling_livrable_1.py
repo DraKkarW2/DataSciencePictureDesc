33bb0111c9b24828f81f9038c335459213a8f24e
DraKkarW2 <68158328+DraKkarW2@users.noreply.github.com>
2024-09-29 13:41:28 +0200

    [add] data_set && livrable_1

---
+++ Data_Wrangling_livrable_1.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+
+
+input_shape = (128, 128, 3)
+batch_size = 32
+epochs = 10
+dataset_dir = r'C:\Users\steve\Documents\GitHub\DataSciencePictureDesc\DataSets\data_set_livrable_1'  
+
+
+datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)  
+
+
+train_generator = datagen.flow_from_directory(
+    dataset_dir,
+    target_size=(128, 128),
+    batch_size=batch_size,
+    class_mode='binary',
+    subset='training'
+)
+
+
+validation_generator = datagen.flow_from_directory(
+    dataset_dir,
+    target_size=(128, 128),
+    batch_size=batch_size,
+    class_mode='binary',
+    subset='validation'
+)
+
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5)) 
+model.add(Dense(64, activation='relu'))
+
+
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+# Entraîner le modèle
+history = model.fit(
+    train_generator,
+    epochs=epochs,
+    validation_data=validation_generator
+)
+
+# Sauvegarder uniquement les poids après l'entraînement
+model.save_weights('model_weights.h5')
+
+# Fonction pour afficher les courbes d'entraînement et de validation
+def plot_training_history(history):
+    plt.figure(figsize=(12, 4))
+    
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'], label='Train Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'], label='Train Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.show()
+
+# Afficher les courbes
+plot_training_history(history)
